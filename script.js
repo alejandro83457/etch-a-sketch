50df73d6cf5077720e789dc27b1e45e51b694ada
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-18 18:17:36 -0800

    Add color chooser and rainbow option

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+let rainbowToggle = false;
+let currentColor = "black";
+
+// Objects
 const grid = document.querySelector("#grid");
+const resetButton = document.querySelector("#reset-button");
+const rainbowButton = document.querySelector("#rainbow-button");
+const colorOptions = document.querySelector("#color-options");
+
+// Event listeners
+resetButton.addEventListener("click", () => resetGrid());
+rainbowButton.addEventListener("click", () => (rainbowToggle = !rainbowToggle));
+colorOptions.addEventListener("change", (e) => {
+    rainbowToggle = 0;
+    currentColor = e.target.value;
+});
+
+// Function decides what color the pixel will be painted as.
+const chooseColor = () => {
+    if (rainbowToggle) {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        return `rgb(${r}, ${g}, ${b})`;
+    } else {
+        return currentColor;
+    }
+};
 
 // Function to generate grid with specific dimension.
 const generateGrid = (dim) => {
@@ -11,6 +38,10 @@ const generateGrid = (dim) => {
 
             const pixel = document.createElement("div");
             pixel.setAttribute("id", "pixel");
+            pixel.addEventListener(
+                "mouseover",
+                (e) => (e.target.style.backgroundColor = chooseColor())
+            );
 
             pixel_col.appendChild(pixel);
         }
@@ -23,6 +54,13 @@ const deleteGrid = () => {
     let grid_cols = document.querySelectorAll("#pixel-col");
     grid_cols.forEach((col) => {
         col.remove();
+    });
+};
+
+const resetGrid = () => {
+    let pixels = document.querySelectorAll("#pixel");
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = "transparent";
     });
 };
 
